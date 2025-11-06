b283a80618756fbd05a621134d0a711c30e89de3
hedy44 <hdiniz4@gmail.com>
2025-11-06 15:35:53 +0000

    Setup base: Material3 and initial Scaffold

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:warranty/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WarrantyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
